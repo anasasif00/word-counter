7e2254be68e38188169fb260269b15dc2942c537
anasasif00 <150178642+anasasif00@users.noreply.github.com>
2023-11-07 10:37:19 -0800

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,37 @@
+import inquirer from 'inquirer'
+
+const counter = (paragraph: string) => {
+  const charCount = paragraph.replace(/\s/g, "").length;
+  const wordCount = paragraph.split(/\s+/).filter(word => word.length > 0).length;
+  const numericCount = paragraph.replace(/\D/g, "").length;
+  const spaceCount = paragraph.split(" ").length - 1; // Count spaces by splitting on space and subtracting 1
+  return `Word Count: ${(wordCount)}\nCharacter Count: ${(charCount)}\nNumeric Character Count: ${(numericCount)}\nSpace Count: ${(spaceCount)}`;
+}
+
+
+
+async function startWordCounter(counter: (text: string) => string) {
+  do {
+      let res = await inquirer.prompt({
+          type: "input",
+          message:("Write your paragraph here : "),
+          name: "paragraph"
+      })
+      console.log(counter(res.paragraph))
+
+      const { continueApp } = await inquirer.prompt({
+          type: 'confirm',
+          name: 'continueApp',
+          message: ('Do you want to continue or Exit ?'),
+          default: true,
+      });
+
+      if (!continueApp) {
+          console.log('Exiting the application.');
+          break;
+      }
+  }
+  while (true)
+}
+
+startWordCounter(counter);
